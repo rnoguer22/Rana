40ff1bee95e544d851eea000100383e5c1a82d5b
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-04 13:54:59 +0100

    Completamos la funcion del primer codigo

---
+++ Suma_simple_matriz.py
@@ -0,0 +1,15 @@
+import math, os, random, re, sys
+
+def simpleArraySum(ar):
+    ar_suma = 0
+    for i in ar:
+        ar_suma += i
+    return ar_suma
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
