556d34d2fd1bcaae42d152c353e5c965cf1a36c8
paulaanb <paulanb2000@gmail.com>
2021-12-14 18:42:29 +0100

    Determinamos las coordenadas de nuestro laberinto

--- Rana.py
+++ Rana.py
@@ -4,3 +4,14 @@ import os
 import random
 import re
 import sys
+
+#Determinamos las coordenadas de nuestro laberinto
+class Coordenadas:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def comparate(self,x,y):
+        if(self.x==x and self.y==y):
+            return True
+        else:
+            return False
