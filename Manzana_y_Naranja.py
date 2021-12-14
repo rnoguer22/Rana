528951663bf2af88f25f4b25b5bb197f57defbdc
paulaanb <paulanb2000@gmail.com>
2021-12-14 18:07:08 +0100

    Definimos la función para contabilizar las manzanas y naranjas

--- Manzana_y_Naranja.py
+++ Manzana_y_Naranja.py
@@ -4,3 +4,16 @@ import os
 import random
 import re
 import sys
+
+#Definimos la función para contabilizar las manzanas y naranjas
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    applesinsidethebox=0
+    orangeinsedethebox=0
+    for apple in apples:
+        if(a+apple>=s and a+apple<=t):
+            applesinsidethebox+=1
+    for orange in oranges:
+        if(b+orange>=s and b+orange<=t):
+            orangeinsedethebox+=1
+    print("Han caido " + str(applesinsidethebox) + " manzanas dentro de la caja.")
+    print("Han caido " + str(orangeinsedethebox) + " naranjas dentro de la caja.")
