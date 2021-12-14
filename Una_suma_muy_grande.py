c49eb7558910ba1066defd5a9d0823a1eefb43ca
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 18:33:59 +0100

    Intoduccion de bucle for De esta manera podemos introducir mas de un numero a la lista

--- Una_suma_muy_grande.py
+++ Una_suma_muy_grande.py
@@ -1,13 +1,18 @@
-import math
-import os, random, re, sys
+import math, os, random, re, sys
 
 def aVeryBigSum(ar):
-    return sum(ar)
+    return sum(ar)   
 
 if __name__ == '__main__':
     fptr = open('OUTPUT_PATH', 'w')
-    ar_count = int(input().strip())
-    ar = list(map(int, input().rstrip().split()))
+    
+    ar_count = int(input("¿Cuantos elementos tiene la lista? ").strip())
+    print ("Introduce los elementos de la lista")
+    ar = []   # En esta lista se almacenaran los numeros
+    for i in range (ar_count):   # Introducimos un bucle para introducir los numeros
+        num = int(input())   # Introducimos un numero
+        ar.append(num)   # El numero se almacena en la lista "ar"
+    
     result = aVeryBigSum(ar)
     fptr.write(str(result) + '\n')
     fptr.close()
