04450670e7080d548c09215a415d0f94239bb75d
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-14 18:55:37 +0100

    Comentarios de explicacion del codigo

--- Suma_simple_matriz.py
+++ Suma_simple_matriz.py
@@ -1,18 +1,18 @@
 import os
 
 def simpleArraySum(ar):
-    return sum(ar)   
+    return sum(ar)   # Devuelve la suma de los elementos de la matriz  
 
 if __name__ == '__main__':
-    fptr = open('OUTPUT_PATH', 'w')
+    fptr = open('OUTPUT_PATH', 'w')   # Abrimos el archivo
     
     ar_count = int(input("Introduzca el tamaño de la matriz: ").strip())
-    ar = []
+    ar = []   # Definimos ar como una lista vacia
     print ("Ahora introduzca los elementos de la matriz:")
     for _ in range (ar_count):
         num = int(input().strip())
-        ar.append(num)
+        ar.append(num)   # De esta manera podemos introducir los numeros a la lista "ar"
     
-    result = simpleArraySum(ar)
-    fptr.write(str(result))
+    result = simpleArraySum(ar)   # Llamamos a la funcion
+    fptr.write(str(result))   # Escribimos la suma de los numeros en el fichero OUTPUT_PATH
     fptr.close()
