9940ea32af7f9a34bc8c3f4b9860dbbcab1f4814
paulaanb <paulanb2000@gmail.com>
2021-12-14 18:11:58 +0100

    Definimos una condición

--- Manzana_y_Naranja.py
+++ Manzana_y_Naranja.py
@@ -15,5 +15,31 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     for orange in oranges:
         if(b+orange>=s and b+orange<=t):
             orangeinsedethebox+=1
-    print("Han caido " + str(applesinsidethebox) + " manzanas dentro de la caja.")
+    print("Han caido " + str(applesinsidethebox) + " manzanas dentro de la caja")
     print("Han caido " + str(orangeinsedethebox) + " naranjas dentro de la caja.")
+    
+#Definimos una condición para contabilizar las manzanas y naranjas 
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
