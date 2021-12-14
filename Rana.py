35d87b4770b332dd0fc872a7cf615eb849cd6ee2
paulaanb <paulanb2000@gmail.com>
2021-12-14 18:47:42 +0100

    Definimos una función para movernos dentro del tunel

--- Rana.py
+++ Rana.py
@@ -20,3 +20,17 @@ class Tunel:
     def __init__(self, x1, y1, x2, y2):
         self.extremo1 = Coordenadas(x1, y1)
         self.extremo2 = Coordenadas(x2, y2)
+        
+#Definimos una funcion para movernos dentro del tunel
+def buscaTunel(Casillax,Casillay,tuneles):
+    coordenadas = Coordenadas(Casillax, Casillay)
+    for t in tuneles:
+        if(t.extremo1.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo2.x
+            coordenadas.y=t.extremo2.y
+            break
+        elif(t.extremo2.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo1.x
+            coordenadas.y=t.extremo1.y
+            break
+    return coordenadas
