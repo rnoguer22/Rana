e2d3d6ed8ee592bd7e8c0c7175282741b7177c64
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-17 22:52:29 +0100

    Copy paste del codigo proporcionado

---
+++ Ejercicios/Juego_piedras.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'gameOfStones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER n as parameter.
+#
+def gameOfStones(n):
+# Write your code here
+if __name__ == '__main__':
+fptr = open(os.environ['OUTPUT_PATH'], 'w')
+t = int(input().strip())
+for t_itr in range(t):
+n = int(input().strip())
+result = gameOfStones(n)
+fptr.write(result + '\n')
+fptr.close()
